b78aa9c47ca33207d005bc825f1db25752b9895a
KananKarim <115145887+KananKarim@users.noreply.github.com>
2024-04-28 16:54:27 +0400

    Update console.py

--- console.py
+++ console.py
@@ -160,7 +160,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 
     HBNBCommand().cmdloop()
